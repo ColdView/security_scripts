84f78e6185ba277fb3296528fdc9332e331ef71a
ColdView <vinnimacc@mail.com>
2017-08-02 16:53:40 +0100

    Updated README.md, script now saves ports based on ip address for multple ips

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
    This script is helpful when evading xss filters.
 
 2. #### nmap_port_extractor.py
-   This script parses a .nmap format scan and extracts the port numbers. It then exports the list of ports to a csv file in the cwd. This list is suitable as an input to a further nmap scan e.g. -p $(cat port-list.txt).
+   This script parses a .nmap format scan and extracts the port numbers and ip addresses. It then exports the list of ports to a csv file under the name of their corresponding ip address, in the cwd. This list is suitable as an input to a further nmap scan e.g. -p $(cat ip-address.txt).
--- nmap_port_extractor.py
+++ nmap_port_extractor.py
@@ -1,17 +1,35 @@
 '''
 Python version 3.5.2
-This script parses a .nmap format scan, extracts the port numbers 
-and exports them to a csv file.
+This script parses a .nmap format scan, extracts the port numbers, 
+ip addresses and exports them to a csv file under the ip's name.
 '''
 import re
 
-ports = []
-path = str(input("Enter the relative or absolute path to the .nmap file: "))
+x = 0
+ips = []
+portlist = 0
+#path = str(input("Enter the relative or absolute path to the .nmap file: "))
+path = "../../code/python/projects/multi_nmap/multi-tcp-port-discovery.nmap"
 
 with open(path, "r") as file_input:
 	for line in file_input:
-		if re.match("^[0-9]+", line):
-			[ports.append(line.split("/")[0])]
+		if "report " in line:
+			ips.append((line.split("for ")[1]).rstrip())
 
-with open("port-list.txt", "w") as file_output:
-	file_output.write(",".join(ports))
+ports = [[] for i in range(len(ips))]
+
+with open(path, "r") as file_input:
+	for line in file_input:		
+		if line == '\n':
+			portlist+=1
+		if re.match("^[0-9]+", line):
+			ports[portlist].append(line.split("/")[0])
+
+# Output FILE FUNCTION
+for i in range(len(ips)):	
+	with open(ips[x], "w") as file_output:
+		file_output.write(",".join(ports[x]))
+	x+=1
+
+print("portlist = ", portlist)
+print(ports)
