f1b415f6904278a7b6562570fa4ce9f740374025
ColdView <vinnimacc@mail.com>
2017-08-12 08:18:49 +0100

    Removed unnecessary variable

--- nmap_port_extractor.py
+++ nmap_port_extractor.py
@@ -1,22 +1,21 @@
 '''
 Python version 3.5.2
 This script parses a .nmap format scan, extracts the port numbers, 
-ip addresses and exports them to a csv file under the ip's name.
+ip addresses and exports the ports to a csv file under the ip's name.
 '''
 import re
 
-x = 0
 ips = []
 portlist = 0
 path = str(input("Enter the relative or absolute path to the .nmap file: "))
-
+#-----------------Pull ips and append to ips list------------------
 with open(path, "r") as file_input:
 	for line in file_input:
 		if "report " in line:
 			ips.append((line.split("for ")[1]).rstrip())
 
 ports = [[] for i in range(len(ips))]
-
+#---------------Pull ports and append to ports list----------------
 with open(path, "r") as file_input:
 	for line in file_input:		
 		if line == '\n':
@@ -24,8 +23,7 @@ with open(path, "r") as file_input:
 		if re.match("^[0-9]+", line):
 			ports[portlist].append(line.split("/")[0])
 
-# Output FILE FUNCTION
+#-----Write port lists to ip files
 for i in range(len(ips)):	
-	with open(ips[x], "w") as file_output:
-		file_output.write(",".join(ports[x]))
-	x+=1
+	with open(ips[i], "w") as file_output:
+		file_output.write(",".join(ports[i]))
